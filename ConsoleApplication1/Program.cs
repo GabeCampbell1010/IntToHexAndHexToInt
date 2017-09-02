12ece830155bb88fae386f89ef2e6c76fd760b8a
GabeCampbell1010 <gabecampbell1010@gmail.com>
2017-09-02 01:36:03 -0600

    added bitwise operator conversion

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,9 +13,9 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             
-            //Console.WriteLine(IntegerToHex.IntToHex(241));
-
-            Console.WriteLine(HexToInteger.HexToInt("FF"));//make sure to check input to not permit letters above F?
+            Console.WriteLine(IntegerToHex.IntToHex(100));
+            
+            //Console.WriteLine(HexToInteger.HexToInt("FF"));//make sure to check input to not permit letters above F?
 
             //Console.WriteLine(IntegerToHex.IntToHex(HexToInteger.HexToInt("A5")));
 
@@ -41,13 +41,41 @@ namespace ConsoleApplication1
             char thisChar;
             List<int> listReversed = new List<int>();
 
-            //populate the list with the correct integer values as per the necessary calculations
+            //variables for the bitwise method
+            List<int> listExperiment = new List<int>();
+            List<int> listExperimentReversed = new List<int>();
+            int valueExperiment = value;
+            int bit;
+            string answerExperiment = "";
+            int k = 4;
+
+            Console.WriteLine("Intitial Integer Value: " + value);
+
+            while (valueExperiment > 0)
+            {
+                bit = valueExperiment & 15;
+                listExperiment.Add(bit);
+                valueExperiment >>= 1 * k;//i think i do need to push it over once every time because i will lose data otherwise, but then compare with 1 still?
+                k--;
+            }
+
             while (quotient != 0)
             {
                 quotient = value / 16;
                 remainder = value % 16;
                 list.Add(remainder);
                 value = quotient;
+            }
+
+            for (int i = 0; i < listExperiment.Count(); i++)
+            {
+                Debug.Assert(listExperiment[i] <= 16 && listExperiment[i] >= 0);
+                if (listExperiment[i] > 9)
+                {
+                    int numChar = (listExperiment[i] - 10 + 65);//of course I could put + 55 here but it is less confusing to convert it first to 10 = 0, 11 = 1, etc...and then add 65 ('A' = 65), so that 10 = 'A' etc... 
+                    listExperiment[i] = (char)numChar;//this (char) cast is actually unnecessary I think
+
+                }
             }
 
             //if a value in the list is greater than 9 then convert it to the apropriate number corresponding to its ascii letter
@@ -69,6 +97,12 @@ namespace ConsoleApplication1
             }
             list = listReversed;
 
+            for (int i = 0; i < listExperiment.Count(); i++)
+            {
+                listExperimentReversed.Add(listExperiment[listExperiment.Count() - 1 - i]);
+            }
+            listExperiment = listExperimentReversed;
+
             //concatenate the whole list into a string and then return that string, taking into account that your above 10 integers must be converted into char-type letters and then into strings to concatenate with the answer string
             foreach (int n in list)
             {
@@ -84,8 +118,24 @@ namespace ConsoleApplication1
                 answer += thisString;//and finall concatenate
             }
 
+            foreach (int n in listExperiment)
+            {
+                if (n < 10)
+                {
+                    thisString = n.ToString();
+                }
+                else
+                {
+                    thisChar = (char)n;//if above 16 then use the temporary variable thisChar to convert the integer to a corresponding char type letter
+                    thisString = thisChar.ToString();//and then make that char letter into a string letter 
+                }
+                answerExperiment += thisString;//and finall concatenate
+            }
+
             //here the test hexValue conversion is used to assert that our answer is correct
             Debug.Assert(answer == hexValue);
+
+            Console.WriteLine("Modulas Answer: " +answer+ " Bitwise Answer: " + answerExperiment);
 
             return answer;
         }

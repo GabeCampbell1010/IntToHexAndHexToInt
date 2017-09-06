3b0233616f170b37745868c7ae1d3a9660525a54
gabecampbell1010 <gabecampbell1010@gmail.com>
2017-09-05 19:36:21 -0600

    got rid of k

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             
-            Console.WriteLine(IntegerToHex.IntToHex(127));
+            Console.WriteLine(IntegerToHex.IntToHex(200));
             
             //Console.WriteLine(HexToInteger.HexToInt("FF"));//make sure to check input to not permit letters above F?
 
@@ -30,7 +30,8 @@ namespace ConsoleApplication1
         {
             //*****THIS FOLLOWING LINE IS DONE FOR TESTING PURPOSES TO ASSERT THAT THE ANSWER IS EQUAL TO THE BUILT IN .NET FUNCTIONALITY FOR INT TO HEX CONVERSION'S ANSWER AT THE END OF THIS METHOD
             string hexValue = value.ToString("X");//THIS IS NOT USED TO DETERMINE THE ANSWER!!!
-            //END TESTING LINES
+                                                  //END TESTING LINES
+
             
 
             int quotient = 1;
@@ -47,16 +48,15 @@ namespace ConsoleApplication1
             int valueExperiment = value;
             int bit;
             string answerExperiment = "";
-            int k = 4;//half the number of bits in the original integer value, so 4 for up thorugh 127
+            //half the number of bits in the original integer value, so 4 for up thorugh 127
 
             Console.WriteLine("Intitial Integer Value: " + value);
 
-            while (valueExperiment > 0)//this is only working for integer values up to a certain amount, the problem is with how to initialize k, if k = 4 then works up to 127, or up until 8 bits are filled, so k is half the number of bits of the value
-            {
+            while (valueExperiment > 0)
+            {//figure out how to add them to a byte?
                 bit = valueExperiment & 15;
                 listExperiment.Add(bit);
-                valueExperiment >>= 1 * k;
-                k--;
+                valueExperiment >>= 4;
             }
 
             while (quotient != 0)
@@ -69,7 +69,7 @@ namespace ConsoleApplication1
 
             for (int i = 0; i < listExperiment.Count(); i++)
             {
-                Debug.Assert(listExperiment[i] <= 16 && listExperiment[i] >= 0);
+                //Debug.Assert(listExperiment[i] <= 16 && listExperiment[i] >= 0);
                 if (listExperiment[i] > 9)
                 {
                     int numChar = (listExperiment[i] - 10 + 65);//of course I could put + 55 here but it is less confusing to convert it first to 10 = 0, 11 = 1, etc...and then add 65 ('A' = 65), so that 10 = 'A' etc... 
@@ -81,7 +81,7 @@ namespace ConsoleApplication1
             //if a value in the list is greater than 9 then convert it to the apropriate number corresponding to its ascii letter
             for (int i = 0; i< list.Count(); i++)
             {
-                Debug.Assert(list[i] <= 16 && list[i] >= 0);
+                //Debug.Assert(list[i] <= 16 && list[i] >= 0);
                 if(list[i] > 9)
                 {
                     int numChar = (list[i] - 10 + 65);//of course I could put + 55 here but it is less confusing to convert it first to 10 = 0, 11 = 1, etc...and then add 65 ('A' = 65), so that 10 = 'A' etc... 
@@ -102,6 +102,9 @@ namespace ConsoleApplication1
                 listExperimentReversed.Add(listExperiment[listExperiment.Count() - 1 - i]);
             }
             listExperiment = listExperimentReversed;
+
+            
+
 
             //concatenate the whole list into a string and then return that string, taking into account that your above 10 integers must be converted into char-type letters and then into strings to concatenate with the answer string
             foreach (int n in list)
@@ -133,7 +136,7 @@ namespace ConsoleApplication1
             }
 
             //here the test hexValue conversion is used to assert that our answer is correct
-            Debug.Assert(answer == hexValue);
+            //Debug.Assert(answer == hexValue);
 
             Console.WriteLine("Modulas Answer: " +answer+ " Bitwise Answer: " + answerExperiment);
 
